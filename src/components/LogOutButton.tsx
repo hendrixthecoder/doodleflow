1f802ecf231bd3318183b8ff9ec2b9519a689021
Johnbull Biobele Glad <95866950+hendrixthecoder@users.noreply.github.com>
2023-12-05 22:05:52 +0100

    fixed type issues

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,16 +1,21 @@
-"use client"
+"use client";
 import { showModal } from "@/lib/utils";
 import Image from "next/image";
-import { useRef } from "react";
+import { RefObject, useRef } from "react";
 
-
-const LogOutButton = ({ dialogRef }) => {
-
+const LogOutButton = ({
+  dialogRef,
+}: {
+  dialogRef: RefObject<HTMLDialogElement>;
+}) => {
   return (
-    <div className="border rounded-full p-1 cursor-pointer" onClick={() => showModal(dialogRef)}>
+    <div
+      className="border rounded-full p-1 cursor-pointer"
+      onClick={() => showModal(dialogRef)}
+    >
       <Image src="/logout.svg" width={23} height={23} alt="Logout" />
     </div>
   );
-}
+};
 
-export default LogOutButton
+export default LogOutButton;
